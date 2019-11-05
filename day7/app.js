67350530aaba85bfea10ca19e6960947bffb45a3
Song Wanathong <58030142@kmitl.ac.th>
2019-11-05 18:02:58 +0700

    test axios

--- day7/app.js
+++ day7/app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var dataroute = require('./route/dataroute')
 var app = express()
+const axios = require('axios');
 
 
 app.use(express.static('public'))
@@ -9,4 +10,16 @@ app.use(bodyParser.json())
 
 app.use('data', dataroute)
 
+axios.get('https://reqres.in/api/users')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+   
+  });
+
 app.listen(3000)
+
