3b778e7ae761576a1289fe192ef5adbf0f726fb9
Song Wanathong <58030142@kmitl.ac.th>
2019-12-05 13:10:58 +0700

    create react project

---
+++ day14/codecamp4/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Popup from './popup'
+function App() {
+  return (
+    <div className="App">
+     
+     <p>Lab</p>
+     <Popup heading='headsssd'></Popup>
+    </div>
+  );
+}
+
+export default App;
---
+++ day14/codecamp4/src/popup.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+class Popup extends React.Component {
+    render() {
+        return (
+            <div className='container'>
+                <div class="card text-center">
+                    <div class="card-header">
+                    <h5 class="card-title">{this.props.heading}</h5>
+
+            </div>
+                    <div class="card-body">
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                       
+                    </div>
+                    <div class="card-footer text-muted">
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default Popup;
